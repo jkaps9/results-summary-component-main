62bc30398d54d736f3a1f5b0116309e77f6a55d0
Josh K Laptop <joshuafkaplan@gmail.com>
2024-09-29 22:38:06 -0400

    styling mostly done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        lightRed: 'hsl(0, 100%, 67%)',
+        veryLightRed: 'hsla(0, 100%, 67%, 0.1)',
+        orangeyYellow: 'hsl(39, 100%, 56%)',
+        veryLightOrangeyYellow: 'hsla(39, 100%, 56%, 0.1)',
+        greenTeal: 'hsl(166, 100%, 37%)',
+        veryLightGreenTeal: 'hsla(166, 100%, 37%, 0.1)',
+        cobaltBlue: 'hsl(234, 85%, 45%)',
+        veryLightCobaltBlue: 'hsla(234, 85%, 45%, 0.1)',
+        
+        // Gradients
+        //// Background
+        lightSlateBlue: 'hsl(252, 100%, 67%)',
+        lightRoyalBlue: 'hsl(241, 81%, 54%)',
+        //// Circle
+        violetBlue: 'hsla(256, 72%, 46%, 1)',
+        persianBlue: 'hsla(241, 72%, 46%, 0)',
+        
+        // Neutral
+        white: 'hsl(0, 0%, 100%)',
+        paleBlue: 'hsl(221, 100%, 96%)',
+        lightLavender: 'hsl(241, 100%, 89%)',
+        darkGrayBlue: 'hsl(224, 30%, 27%)',
+
+      },
+      fontFamily: {
+        sans: ['Hanken Grotesk', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
